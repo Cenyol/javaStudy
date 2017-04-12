8ff2a58ffd74f799528acd2d109639f99ce8a2d1
Cenyol <cenyol@163.com>
2017-04-12 15:22:01 +0800

    add my java io input decorator

---
+++ study/src/main/com/cenyol/study/designPartner/P02_DecoratorPattern/MyJavaIoDecorator/LowerCaseInputStream.java
@@ -0,0 +1,36 @@
+package com.cenyol.study.designPartner.P02_DecoratorPattern.MyJavaIoDecorator;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by cenyol on 12/04/2017.
+ */
+public class LowerCaseInputStream extends FilterInputStream {
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+    public LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    public int read() throws IOException{
+        int c = super.read();
+        return c == -1 ? c : Character.toLowerCase(c);
+    }
+
+    public int read(byte[] b, int offset, int len) throws IOException{
+        int result = super.read(b, offset, len);
+        for (int i = result; i < offset + result; i++){
+            b[i] = (byte)Character.toLowerCase((char)b[i]);
+        }
+        return result;
+    }
+}
