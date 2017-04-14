131cb3d820659cb8f2943b9db87487d8ed415031
Cenyol <cenyol@163.com>
2017-04-14 14:35:45 +0800

    删掉版本控制库中的idea文件夹

--- study/src/main/com/cenyol/study/DesignPattern/P05_CommandPattern/Client.java
+++ study/src/main/com/cenyol/study/DesignPattern/P05_CommandPattern/Client.java
@@ -3,11 +3,8 @@ package com.cenyol.study.DesignPattern.P05_CommandPattern;
 /**
  * Created by cenyol on 15/03/2017.
  */
-public class Test {
+public class Client {
 
     public static void main(String[] args){
-        System.out.println((byte)-1);
-        System.out.println((char)(byte)-1);
-        System.out.println((int)(char)(byte)-1);
     }
 }
