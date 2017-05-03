0a67681ae8a39b687e63da0c48da5fb7c2f57660
Cenyol <cenyol@163.com>
2017-05-03 21:02:22 +0800

    添加测试TCP粘包的代码

--- study/src/main/com/cenyol/study/netty/TimeServerHandler.java
+++ study/src/main/com/cenyol/study/netty/TimeServerHandler.java
@@ -1,28 +1,34 @@
 package com.cenyol.study.netty;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.util.Date;
 
 /**
  * Created by cenyol on 02/05/2017.
  */
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+    private int counter = 0;
 
     @Override
-    public void channelActive(final ChannelHandlerContext ctx) { // (1)
-        final ByteBuf time = ctx.alloc().buffer(4); // (2)
-        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+    public void channelActive(final ChannelHandlerContext ctx) {
 
-        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
-        f.addListener(new ChannelFutureListener() {
-            public void operationComplete(ChannelFuture future) {
-                assert f == future;
-                ctx.close();
-            }
-        }); // (4)
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        byte[] req = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(req);
+        String body = new String(req, "UTF-8").substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("The time server receive order : " + body + "; the counter is " + ++counter);
+        String currentTime = "QUERY TIME ORD".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime += System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
     }
 
     @Override
